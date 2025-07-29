3d0f0a491374e49c4e15b3a26531d17ccc7d8a40
Farooq Ahmed <03174748082ali@gmail.com>
2025-07-29 02:41:20 -0700

    work completed

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
 document.write("<h1>" + "Vowel Letter Checker" + "<h1/>");
+
+// function countVowel(str){
+//     let count = 0;
+//     for(let char of str){
+//         if(char === "a" ||
+//             char === "e" ||
+//             char === "i" ||
+//             char === "o" ||
+//             char === "u"
+//         ){
+//             count++
+//         }
+//     }
+    
+//     document.write("<h1>" + count + "<h1/>");
+// }
+// countVowel("apna college");
+
+
+// const countVow = (str) =>{
+//     let count = 0; 
+//     for(let char of str){
+//         if(char === "a" ||
+//             char === "e" ||
+//             char === "i" ||
+//             char === "o" ||
+//             char === "u"
+//         ){
+//             count++;
+//         }
+//     }
+//        document.write("<h1>" + count + "<h1/>");
+// }
+
+// countVow("Pakistan Zindabad");
